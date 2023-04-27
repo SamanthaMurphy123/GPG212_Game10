641fb08bea686544c0006ada30360f1682e76a2b
MicroSillyWilly <1025946@student.sae.edu.au>
2023-04-27 12:26:56 +1000

    terrerain and dialogue integration

---
+++ Assets/Scripts/RecyledDialogue.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecyledDialogue : MonoBehaviour
+{
+    public GameObject uiCanvas; // Assign the UI canvas to this in the Inspector
+    private bool canSpawnUI = false;
+    public GameObject EToEnable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EToEnable.SetActive(true);
+        if (other.CompareTag("Player"))
+        {
+
+            canSpawnUI = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canSpawnUI = false;
+            uiCanvas.SetActive(false); // Deactivate the UI canvas when the player leaves the collision box
+            EToEnable.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (canSpawnUI && Input.GetKeyDown(KeyCode.F))
+        {
+            uiCanvas.SetActive(true); // Activate the UI canvas when the player presses E
+            EToEnable.SetActive(false);
+        }
+    }
+
+    public void Disable()
+    {
+        EToEnable.SetActive(false);
+    }
+}
